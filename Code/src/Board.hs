e9316b1dcbbdbdd44982beda0c2bc47fa26778a2
keananf <keananf@live.com>
2016-04-07 19:31:45 +0100

    Functionality for making moves
    No graphics yet

--- Code/src/Board.hs
+++ Code/src/Board.hs
@@ -1,13 +1,14 @@
 module Board where
 
+import Data.Maybe
+import Data.List
+
 data Col = Black | White
   deriving (Show, Eq)
 
-other :: Col -> Col
-other Black = White
-other White = Black
-
 type Position = (Int, Int)
+type Piece = (Position, Col)
+
 
 -- A Board is a record containing the board size (a board is a square grid, n *
 -- n), the number of consecutive passes, and a list of pairs of position and
@@ -15,14 +16,9 @@ type Position = (Int, Int)
 
 data Board = Board { size :: Int,
                      passes :: Int,
-                     pieces :: [(Position, Col)]
+                     pieces :: [Piece]
                    }
   deriving Show
-
--- Default board is 8x8, neither played has passed, with 4 initial pieces 
-initBoard :: Board
-initBoard = Board 8 0 [((3,3), Black), ((3, 4), White),
-                       ((4,3), White), ((4,4), Black)]
 
 -- Overall state is the board and whose turn it is, plus any further
 -- information about the world (this may later include, for example, player
@@ -31,18 +27,99 @@ initBoard = Board 8 0 [((3,3), Black), ((3, 4), White),
 -- Feel free to extend this, and 'Board' above with anything you think
 -- will be useful (information for the AI, for example, such as where the
 -- most recent moves were).
-data World = World { board :: Board,
+data World = World { gameboard :: Board,
                      turn :: Col }
+
+
+-- Default board is 8x8, neither played has passed, with 4 initial pieces 
+initBoard :: Board
+initBoard = Board 8 0 [((3,3), White), ((3,4), Black),
+                       ((4,3), Black), ((4,4), White)]
 
 initWorld :: World
 initWorld = World initBoard Black
 
+directions :: [Position]
+directions =  [(x,y )| x <- [-1..1], y <- [-1..1]]
+
+changeColour :: Col -> Piece -> Piece
+changeColour col (pos,_) = (pos, col)
+
+other :: Col -> Col
+other Black = White
+other White = Black
+
+--------------------------------------------------------------------
 -- Play a move on the board; return 'Nothing' if the move is invalid
 -- (e.g. outside the range of the board, there is a piece already there,
 -- or the move does not flip any opposing pieces)
-makeMove :: Board -> Col -> Position -> Maybe Board
-makeMove = undefined
+makeMove :: World -> Position -> Maybe World
+makeMove world pos | not (isOccupied board pos) && (inRange board pos) =
+                       Just newWorld {gameboard = newBoard}
+                   | otherwise = Nothing
+  where board = (gameboard world)
+        col = (turn world)
+        pieces = flatten (flipPieces board col pos)
+        newBoard = updatePieces board pieces pos col
+        newWorld = changeTurn world
 
+changeTurn :: World -> World
+changeTurn world = world {turn = (other col)}
+  where col = turn world
+
+updatePieces :: Board -> [Piece] -> Position -> Col -> Board
+updatePieces board newPieces pos col = board {pieces = allPieces}
+  where otherPieces = deleteFirstsBy (\x y -> ((fst x) == (fst y))) (pieces board) (newPieces)
+        newPiece = (pos, col)
+        allPieces = newPiece:newPieces ++ otherPieces
+
+-- | Flatten 2D list of pieces which results from flipPieces and FindPieces
+-- | It is 2D to represent all the pieces to flip in all directions
+flatten :: [[Piece]] -> [Piece]
+flatten [] = []
+flatten (p:ps) = p ++ (flatten ps)
+
+-- | Flip the colours of all pieces returned from findPieces
+flipPieces :: Board -> Col -> Position -> [[Piece]]
+flipPieces board col pos = flippedPieces
+  where allPieces = (findPieces board col pos directions)
+        piecesToFlip = (filter (\list -> not (null list) && snd (last list) == col) allPieces) --eliminate directions which don't have the same colour at the end 
+        flippedPieces =map (\list -> map (changeColour col) list) piecesToFlip
+
+-- | Finds pieces to flip in each direction
+findPieces :: Board -> Col -> Position -> [Position] -> [[Piece]]
+findPieces board col pos [] = []
+findPieces board col pos (d:ds) = pieces:(findPieces board col pos ds)
+  where maybePieces = searchDirection board pos d col --find infinite list of maybe pieces in d
+        justPieces = takeWhile (isJust) maybePieces --get rid of all nothings
+        pieces = map (fromJust) justPieces --all pieces in this direction
+
+
+-- | Retrieves an infinite list of maybe pieces in this direction
+searchDirection :: Board -> Position -> Position -> Col -> [Maybe Piece]
+searchDirection board (x,y) (x',y') col = piece:pieces
+  where piece = findPiece (board) (x+x', y+y')
+        pieces = (searchDirection board (x+x', y+y') (x',y') col)
+
+-- | Finds a maybe piece in the given direction.
+-- | Used to find the infinite list for lazy evaluation
+findPiece :: Board -> Position -> Maybe Piece
+findPiece board pos | length list == 1 = Just (head list)
+                    | otherwise = Nothing
+  where list = filter (\piece -> (fst piece) == pos) (pieces board)
+
+-- | Check if the space is occupied
+isOccupied :: Board -> Position -> Bool
+isOccupied board pos = any (== pos) positions
+  where positions = map (fst) (pieces board)
+
+-- | Check if the desired position is in range of the board
+inRange :: Board -> Position -> Bool
+inRange board (x,y) = x < len && x >= 0 && y < len && y >= 0
+  where len = (size board)
+-----------------------------------------------------------------
+
+-----------------------------------------------------------------
 -- Check the current score
 -- Returns a pair of the number of black pieces, and the number of
 -- white pieces
@@ -55,6 +132,7 @@ countPieces :: (Int, Int) -> [Col] -> (Int, Int)
 countPieces (x,y) [] = (x,y)
 countPieces (x,y) (z:zs) | Black == z = countPieces (x+1,y) zs
                          | otherwise = countPieces (x,y+1) zs
+-----------------------------------------------------------------
 
 
 -- Return true if the game is complete (that is, either the board is
